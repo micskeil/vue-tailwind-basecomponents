d5d3ef67e8f2bc58c8768695a7c915a16b8549f5
micskeil <micskeil@gmail.com>
2023-04-24 13:27:15 +0200

    fix/dts-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import dts from '@vitejs/plugin-dts';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
