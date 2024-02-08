9828329d0049dc35c54e9de634287ad0be090028
Lali Micskei <micskeil@gmail.com>
2024-02-08 20:05:58 +0100

    Delete examples Use only ES module version

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import VBaseButton from './components/VBaseButton.vue';
 import VBaseCode from './components/VBaseCode.vue';
 import VBaseDialog from './components/VBaseDialog.vue';
+import VBaseInput from './components/VBaseInput.vue';
 import VBaseFileInput from './components/VBaseFileInput.vue';
 import VBaseIcon from './components/VBaseIcon.vue';
 import VBaseTabs from './components/VBaseTabs.vue';
@@ -11,9 +12,10 @@ import '@/assets/main.css';
 export {
   VBaseButton,
   VBaseCode,
+  VBaseDialog,
+  VBaseInput,
   VBaseFileInput,
   VBaseIcon,
   VBaseTextarea,
-  VBaseDialog,
   VBaseTabs,
 };
